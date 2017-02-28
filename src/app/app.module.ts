fc7a09c91bb7b390209777848aa2dae95cec6939
Kelly Moore <kellygarskemoore@gmail.com>
2017-02-27 23:08:17 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }	 from '@angular/forms';
+import { HttpModule }	 from '@angular/http';
 
-import { AppComponent }  from './app.component';
+import { CollectiblesComponent }  from './collectible';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+imports: [ BrowserModule,
+		   FormsModule,
+		   HttpModule 
+		 ],
+  providers: [],
+  declarations: [ CollectiblesComponent ],
+  bootstrap:    [ CollectiblesComponent ]
 })
 export class AppModule { }
---
+++ src/app/collectible.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'collectibles-app',
+	templateUrl: 'template.html',
+	styleUrls: ['collectibles.css']
+})
+export class CollectiblesComponent {
+	newCollectible: string;
+	collectibles: any;
+	collectibleObj: any;
+
+	constructor() {
+		this.newCollectible = '';
+		this.collectibles = [];
+	}
+
+	addCollectible() {
+		this.collectibleObj = {
+			newCollectible: this.newCollectible,
+			catalogued: false
+		}
+		this.collectibles.push(this.collectibleObj);
+		this.newCollectible = '';
+		event.preventDefault();
+	}
+
+	deleteCollectible(index: any) {
+		this.collectibles.splice(index, 1);
+	}
+
+	deleteSelectedCollectibles() {
+		for(var i=(this.collectibles.length -1); i > -1; i--) {
+			if(this.collectibles[i].catalogued) {
+				this.collectibles.splice(i,1);
+			}
+		}
+	}
+}
